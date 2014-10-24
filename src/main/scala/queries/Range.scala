ee801fa0954964c59f29d58d96e96f6910ae30e3
Thamir Qadah <qadah.thamir@gmail.com>
2014-10-24 00:20:04 -0400

    Streamed output for Range.scala

--- src/main/scala/queries/Range.scala
+++ src/main/scala/queries/Range.scala
@@ -20,18 +20,20 @@
 package queries
 
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.{ Seconds, StreamingContext }
 import org.apache.spark.streaming.StreamingContext._
 import org.apache.spark.storage.StorageLevel
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
-
-
+import spray.json.JsObject
+import spray.json.JsNumber
+import spray.json.JsString
+import utils.OutputHelper
 
 object Range extends Serializable {
 
-    Logger.getLogger("org").setLevel(Level.WARN)
-    Logger.getLogger("akka").setLevel(Level.WARN)
+  Logger.getLogger("org").setLevel(Level.WARN)
+  Logger.getLogger("akka").setLevel(Level.WARN)
 
   def main(args: Array[String]) {
     if (args.length < 2) {
@@ -68,23 +70,22 @@ object Range extends Serializable {
 
     // Temporal Range
 
-    case class point(x:Double,y:Double) extends Serializable {
+    case class point(x: Double, y: Double) extends Serializable {
       override def toString() = {
 
-        "X-Cor:" + x.toString()+" Y-Coordniate: "+y.toString()
+        "X-Cor:" + x.toString() + " Y-Coordniate: " + y.toString()
       }
     }
 
-
-    case class queryRegion(min:point,max:point) extends Serializable {
+    case class queryRegion(min: point, max: point) extends Serializable {
       override def toString() = {
-        min+" "+max
+        min + " " + max
       }
 
     }
-    case class tweete(ID:Double, location:point, tText:Array[String],tweetL:Int, tTextL:Int, tTime:String) extends Serializable{
+    case class tweete(ID: Double, location: point, tText: Array[String], tweetL: Int, tTextL: Int, tTime: String) extends Serializable {
 
-      override def equals(that:Any):Boolean = {
+      override def equals(that: Any): Boolean = {
         true
       }
 
@@ -93,8 +94,7 @@ object Range extends Serializable {
     // Create a list for current tweets
     var tx: Set[tweete] = Set()
 
-
-    var i =0
+    var i = 0
     // Parse Stream and Create Tweet tuple(s)
     val tweets = tweets_window.map(line => {
       val tweet = line.split(" ")
@@ -106,14 +106,13 @@ object Range extends Serializable {
       var tText = tweet.slice(5, tweet.length)
       var location = point(0, 0)
       var ID = 0.0
-      var Time:Long=0
+      var Time: Long = 0
       try {
         //Time = tTime.toLong
         location = point(tLat.toDouble, tLong.toDouble)
         ID = tID.toDouble
 
-      }
-      catch {
+      } catch {
         case e: Exception => {
           Time = 0
           ID = 0.0
@@ -122,17 +121,17 @@ object Range extends Serializable {
         }
       }
 
-      val queryCandidate:tweete = new tweete(ID, location, tText, tweet.length, tText.length, tTime)
-      val oneList:Set[tweete] = Set(queryCandidate)
+      val queryCandidate: tweete = new tweete(ID, location, tText, tweet.length, tText.length, tTime)
+      val oneList: Set[tweete] = Set(queryCandidate)
       val tmp = tx
       tx = tmp.+(queryCandidate)
-      i = i+1
+      i = i + 1
 
-      (1,queryCandidate)
+      (1, queryCandidate)
     })
-    var temp:StringBuilder = new StringBuilder()
-    var temp2:StringBuilder = new StringBuilder()
-    var d:Boolean = false
+    var temp: StringBuilder = new StringBuilder()
+    var temp2: StringBuilder = new StringBuilder()
+    var d: Boolean = false
     val tweets2 = tweets.join(tweets).flatMap({
       case (_, (p, q)) => {
 
@@ -140,14 +139,14 @@ object Range extends Serializable {
           val part1 = (p.location.x.toDouble - q.location.x.toDouble) * (p.location.x.toDouble - q.location.x.toDouble)
           val part2 = (p.location.y.toDouble - q.location.y.toDouble) * (p.location.y.toDouble - q.location.y.toDouble)
 
-          var Tdistance:Double = 0// p.tTime.toDouble - q.tTime.toDouble
+          var Tdistance: Double = 0 // p.tTime.toDouble - q.tTime.toDouble
 
           val distance = math.sqrt(part1 + part2)
           if (distance < 10) {
 
             p.tText.distinct.foreach(x => {
               for (y <- q.tText.distinct) {
-                if (x.equals(y)){
+                if (x.equals(y)) {
                   temp.append(x)
                   temp.append(", ")
                 }
@@ -155,62 +154,87 @@ object Range extends Serializable {
             })
 
             d = false
-            if(!temp.isEmpty){
+            if (!temp.isEmpty) {
               temp2 = temp
-              temp= new StringBuilder("")
+              temp = new StringBuilder("")
               d = true
 
             }
-            Some(p, q, "Spatial Distance=" + distance +  " Keyword Matches= " + temp2, d, Tdistance)
+            Some(p, q, "Spatial Distance=" + distance + " Keyword Matches= " + temp2, d, Tdistance)
 
-          }
-          else
+          } else
             None
-        }
-        else None
+        } else None
       }
 
     })
 
+    val output = tweets2.map(f => {
+      val sb1 = new StringBuilder()
+      val sb2 = new StringBuilder()
 
-
-
-    tweets2.foreachRDD(t => t.foreach(f => {
-      // Printing Tweet 1
-      if(f._4) {
-        println(Console.WHITE +"First Tweet")
-        println("Tweet ID:" + f._1.ID.toLong + "  Tweet Length:" + f._1.tweetL + "  Text Length:" + f._1.tTextL)
-        println("Tweet Long:" + f._1.location.x.toDouble + "  Tweet Lat:" + f._1.location.y.toDouble)
-        //println("Tweet Time: " + f._1.tTime)
-        print("TweetText:")
-        for (x <- f._1.tText) {
-          print(" " + x)
-        }
-        println()
-
-        // Printing Tweet 2
-        println(Console.WHITE + "Second Tweet")
-        println("Tweet ID:" + f._2.ID.toLong + "  Tweet Length:" + f._2.tweetL + "  Text Length:" + f._2.tTextL)
-        println("Tweet Long:" + f._2.location.x.toDouble + "  Tweet Lat:" + f._2.location.y.toDouble)
-        //println("Tweet Time: " + f._2.tTime)
-        print("TweetText:")
-        for (y <- f._2.tText) {
-          print(" " + y)
-        }
-        println()
-
-        print(Console.BOLD + "Matching Details: ")
-        println(f._3)
-
-        println(Console.WHITE + "===========================================")
-        println()
-
+      for (x <- f._1.tText) {
+        sb1.append(" " + x)
       }
 
-    }))
+      for (x <- f._2.tText) {
+        sb2.append(" " + x)
+      }
 
+      JsObject(
+        "id1" -> JsNumber(f._1.ID.toLong),
+        "lat1" -> JsNumber(f._1.location.y.toDouble),
+        "lng1" -> JsNumber(f._1.location.x.toDouble),
+        "text1" -> JsString(sb1.toString),
+        "id2" -> JsNumber(f._1.ID.toLong),
+        "lat2" -> JsNumber(f._2.location.y.toDouble),
+        "lng2" -> JsNumber(f._2.location.x.toDouble),
+        "text2" -> JsString(sb2.toString),
+        "mdetails" -> JsString(f._3)).toString
+    })
 
+    output.print
+    
+    val outputWriter = new OutputHelper[String]()
+    outputWriter.writeOutput(output)(x=>x)
 
+    //    tweets2.foreachRDD(t => t.foreach(f => {
+    //      // Printing Tweet 1
+    //      
+    //      if(f._4) {
+    //        println(Console.WHITE +"First Tweet")
+    //        println("Tweet ID:" + f._1.ID.toLong + "  Tweet Length:" + f._1.tweetL + "  Text Length:" + f._1.tTextL)
+    //        println("Tweet Long:" + f._1.location.x.toDouble + "  Tweet Lat:" + f._1.location.y.toDouble)
+    //        //println("Tweet Time: " + f._1.tTime)
+    //        print("TweetText:")
+    //        for (x <- f._1.tText) {
+    //          print(" " + x)
+    //        }
+    //        println()
+    //        
+    //                
+    //
+    //        // Printing Tweet 2
+    //        println(Console.WHITE + "Second Tweet")
+    //        println("Tweet ID:" + f._2.ID.toLong + "  Tweet Length:" + f._2.tweetL + "  Text Length:" + f._2.tTextL)
+    //        println("Tweet Long:" + f._2.location.x.toDouble + "  Tweet Lat:" + f._2.location.y.toDouble)
+    //        //println("Tweet Time: " + f._2.tTime)
+    //        print("TweetText:")
+    //        for (y <- f._2.tText) {
+    //          print(" " + y)
+    //        }
+    //        println()
+    //
+    //        print(Console.BOLD + "Matching Details: ")
+    //        println(f._3)
+    //
+    //        println(Console.WHITE + "===========================================")
+    //        println()
+    //                
+    //
+    //      }
+    //
+    //    }))
 
     ssc.start()
     ssc.awaitTermination()
